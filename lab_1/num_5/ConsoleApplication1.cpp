9ef0092fb1b92d61b29ef06a31517426f29012d8
iragrom <70807205+iragrom@users.noreply.github.com>
2025-11-06 16:41:34 +0300

    Add files via upload

---
+++ lab_1/num_5/ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main()
+{
+  int n;
+    cin >> n;
+    vector<int> nums(n);
+    for(int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+    int min_val = *min_element(nums.begin(), nums.end());
+    int count = 0;
+    for(int num : nums) {
+        if(num == min_val) count++;
+    }
+    cout << count << endl;
+    return 0;
+}
